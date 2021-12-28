fc577c549151e22f3725193480d0508c70d08eb2
DieniK <dieni.silva@solinftec.com>
2021-12-28 13:42:48 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> 
-      <v-app-bar-title>Vue+ Vuetify</v-app-bar-title>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-title>Trainee</v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2" >{{ link }}</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
     </v-app-bar>
-
-    <!-- Login -->
     <v-main>
-      <v-card width="500" class="mx-auto mt-5">
-        <v-card-title>Login</v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-            <v-text-field label="Password" prepend-icon="mdi-lock" :append-icon="exibir ? 'mdi-eye' : 'mdi-eye-off'" :type="exibir ? 'text' : 'password'" @click:append="() => (exibir = !exibir)"></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2" > {{ link }} </v-btn>
-        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12" > {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-col>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
       </v-row>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
-    exibir: false,
-    links:["Home", "Login","Cadastro"]
+    links: ["Home", "Login", "API"],
   }),
+
+  methods: {},
 };
 </script>
